902af28858a5a81d9685d4e3be1e93176354be8d
emberhawk <70652115+emberhawk@users.noreply.github.com>
2020-12-17 16:27:45 +0000

    Unit Lists
    Refactored Unit script
    Added Server and Client Events
    
    Added RTSPlayer Script
    Added Server Events and assigned corresponding methods
    Added Client Events and assigned corresponding methods
    Added script to player prefab

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,104 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    [SerializeField] private List<Unit> myUnits = new List<Unit>();
+
+
+
+    // Server and client Below
+
+    #region Server
+
+    // Server Events 
+    public override void OnStartServer()
+    {
+        // listen for these events and pass them to the method
+        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+    }
+
+    public override void OnStopServer()
+    {
+        // stop listening for these events
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+    }
+
+    // Server Methods 
+    private void ServerHandleUnitSpawned(Unit unit)
+    {
+        // check to make sure the unit call belongs to the owner 
+        if(unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        // add the unit to my unit list
+        myUnits.Add(unit);
+    }
+
+    private void ServerHandleUnitDespawned(Unit unit)
+    {
+        // check to make sure the unit call belongs to the owner 
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        // remove the unit from my unit list
+        myUnits.Remove(unit);
+    }
+
+    #endregion
+    //
+    // space
+    //
+    #region Client
+
+    // Client Events
+
+    public override void OnStartClient()
+    {
+        // only client can call
+        if (!isClientOnly) { return; }
+
+        // listen for these events and pass them to the method
+        Unit.AuthorityOnUnitSpawned += AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned += AuthorityHandleUnitDespawned;
+    }
+
+    public override void OnStopClient()
+    {
+        // only client can call
+        if (!isClientOnly) { return; }
+
+        // listen for these events and pass them to the method
+        Unit.AuthorityOnUnitSpawned -= AuthorityHandleUnitSpawned;
+        Unit.AuthorityOnUnitDespawned -= AuthorityHandleUnitDespawned;
+    }
+
+
+    // Client Methods
+
+    private void AuthorityHandleUnitSpawned(Unit unit)
+    {
+        // protection check
+        if (!hasAuthority) { return; }
+
+        // add the unit to my unit list
+        myUnits.Add(unit);
+    }
+
+    private void AuthorityHandleUnitDespawned(Unit unit)
+    {
+        // protection check
+        if (!hasAuthority) { return; }
+
+        // remove the unit from unit list
+        myUnits.Remove(unit);
+    }
+
+
+    #endregion
+
+
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,4 +1,5 @@
 ﻿using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,8 +11,13 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
+    // Server Events
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+    // Clients Events
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
 
-    
 
     // Getter to return UnitMovement to other Classes
     public UnitMovement GetUnitMovement()
@@ -21,8 +27,48 @@ public class Unit : NetworkBehaviour
 
     // Client and Server below
 
+    #region Server
+
+    public override void OnStartServer()
+    {
+        // tell the server about this unit by subscribing to an event
+        
+        ServerOnUnitSpawned?.Invoke(this);
+
+    }
+
+    public override void OnStopServer()
+    {
+        // tell the server about this unit by subscribing to an event
+        
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+
+    #endregion
+
     #region Client
 
+    public override void OnStartClient()
+    {
+        // if we are server or dont have authority do nothing
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+
+    public override void OnStopClient()
+    {
+        // if we are server or dont have authority do nothing
+        if (!isClientOnly || !hasAuthority) { return; }
+
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
+
+
+
+    // client method to toggle select circle
     [Client]
     public void Select()
     {
@@ -30,6 +76,7 @@ public class Unit : NetworkBehaviour
         onSelected?.Invoke();
     }
 
+    // client method to toggle deselect circle
     [Client]
     public void Deselect()
     {
