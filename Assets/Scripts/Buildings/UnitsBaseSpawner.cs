90775806f363f483f69648fb3cb69b25ee1f76b2
emberhawk <70652115+emberhawk@users.noreply.github.com>
2020-12-17 13:55:45 +0000

    Spawners
    Changed Player to Unit
    Added Player prefab
    Added UnitSpawnerPrefab
    Refactored Network Manager
    Connected Prefabs

---
+++ Assets/Scripts/Buildings/UnitsBaseSpawner.cs
@@ -0,0 +1,52 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitsBaseSpawner : NetworkBehaviour , IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab = null;
+    [SerializeField] private Transform unitSpawnPoint = null;
+
+
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        // instance of a spawned prefab
+        GameObject unitInstance = Instantiate(unitPrefab, unitSpawnPoint.position, unitSpawnPoint.rotation);
+
+        // tell the server to spawn with client authority 
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+
+    }
+
+
+
+    #endregion
+
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // Only Left Clicks
+        if(eventData.button != PointerEventData.InputButton.Left) { return; }
+
+        // client has the authority
+        if (!hasAuthority){ return; }
+
+        // send Spawn unit command to server
+        CmdSpawnUnit();
+    }
+
+
+
+    #endregion
+
+
+
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
+    [SerializeField] private GameObject unitBaseSpawnerPrefab = null;
+
+
 
 
 
     #region Server
 
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
 
+        // an instance of red cube
+        GameObject unitBaseSpawnerInstance = Instantiate(unitBaseSpawnerPrefab, conn.identity.transform.position, conn.identity.transform.rotation);
 
+        // tell the server about spawning a base for other spawns with the player authority
+        NetworkServer.Spawn(unitBaseSpawnerInstance, conn);
 
-
-
+    }
 
 
 
