b34444566b5b6c4495a36982efa516744028330d
emberhawk <70652115+emberhawk@users.noreply.github.com>
2020-12-17 14:51:51 +0000

    Highlighting a Unit
    Added a green selection sprite to Unit
    Added Selection Handler Gameobject & Script.
    Script toggles unit selection and toggles unity events
    Added a ground material

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,34 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] private UnityEvent onSelected = null;
+    [SerializeField] private UnityEvent onDeselected = null;
+
+
+    #region Client
+
+    [Client]
+    public void Select()
+    {
+        if (!hasAuthority) { return; }
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!hasAuthority) { return; }
+        onDeselected?.Invoke();
+    }
+
+
+
+    #endregion
+
+
+}
---
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UnitSelectionHandler : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask = new LayerMask(); 
+    private Camera mainCamera;
+
+    private List<Unit> selectedUnits = new List<Unit>();
+
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            foreach (Unit selectedUnit in selectedUnits)
+            {
+                selectedUnit.Deselect();
+            }
+
+            // clear the list
+            selectedUnits.Clear();
+
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            ClearSelectionArea();
+        }
+
+    }
+
+    private void ClearSelectionArea()
+    {
+        // ray from mouse point
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        // return what the raycast hit regading the layermask
+        if(!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask))
+        {
+            return;
+        }
+
+        // test if the ray hits a unit  
+        if(!hit.collider.TryGetComponent(out Unit unit)) { return; }
+
+        // remove a selection that does not belong to us
+        if (!unit.hasAuthority) { return; }
+
+        selectedUnits.Add(unit);
+
+        // Select all units in the list  that have a collider to have a green selection circle toggled
+        foreach (Unit selectedUnit in selectedUnits)
+        {
+            selectedUnit.Select();
+        }
+    }
+}
