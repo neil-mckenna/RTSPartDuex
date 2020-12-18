312cec04131a503a94c1df7211d29eba06658f37
emberhawk <70652115+emberhawk@users.noreply.github.com>
2020-12-18 13:01:27 +0000

    Healthbar UI
    Created a healthDisplay Script
    Created a Healthbar UI prefab
    
    Added healthbar to base and unit prefabs
    
    Changed input system to both for mouseovers inputs

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -8,10 +8,18 @@ public class Health : NetworkBehaviour
 {
     [SerializeField] private int maxHealth = 100;
 
-    [SyncVar]
+    [SyncVar(hook = nameof(HandleHealthUpdated))]
     private int currentHealth;
 
+
+    // server actions
     public event Action ServerOnDie;
+
+
+    // client actions
+    public event Action<int, int> ClientOnHealthUpdated; 
+
+
 
     #region Server
 
@@ -45,6 +53,12 @@ public class Health : NetworkBehaviour
 
 
     #region Client
+
+    private void HandleHealthUpdated(int oldhealth, int newhealth)
+    {
+        ClientOnHealthUpdated?.Invoke(newhealth, maxHealth);
+    }
+
 
     #endregion
 
---
+++ Assets/Scripts/Combat/HealthDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] private Health health = null;
+    [SerializeField] private GameObject healthBarParent = null;
+    [SerializeField] private Image healthBarImage = null;
+
+    private void Awake()
+    {
+        // subscribe to the event on start of game
+        health.ClientOnHealthUpdated += HandleHealthUpdated;
+
+    }
+
+    private void OnDestroy()
+    {
+        // on application destruction unsubscribe to the event
+        health.ClientOnHealthUpdated -= HandleHealthUpdated;
+    }
+
+    private void OnMouseEnter()
+    {
+        // show the healthbars when moved over
+        healthBarParent.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        // hide the healthbars when moved over
+        healthBarParent.SetActive(false);
+    }
+
+    private void HandleHealthUpdated(int currentHealth, int maxhealth)
+    {
+        // the float cast change from decimal to int
+        healthBarImage.fillAmount = (float)currentHealth / maxhealth;
+    }
+
+
+}
