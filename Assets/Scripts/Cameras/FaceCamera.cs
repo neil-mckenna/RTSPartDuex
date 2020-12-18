0f6739644bedcf7a60acc2cfd6bee66d7f338013
emberhawk <70652115+emberhawk@users.noreply.github.com>
2020-12-18 16:36:31 +0000

    Building PreSetup
    Added FaceCamera Script for healthbars
    Changed the Physics collison and layers
    Assigned the layers to the units , floor, projectiles

---
+++ Assets/Scripts/Cameras/FaceCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+
+
+    void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + mainCameraTransform.rotation * Vector3.forward,
+            mainCameraTransform.rotation * Vector3.up
+            );
+    }
+
+
+}
