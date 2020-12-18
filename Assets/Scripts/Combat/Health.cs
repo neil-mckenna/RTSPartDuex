c105a2b30c75be6893769983240467b9d39f4bbc
emberhawk <70652115+emberhawk@users.noreply.github.com>
2020-12-18 11:59:41 +0000

    Health Component
    Added Health Script
    Added a damage method to Unit Projectile
    Added script to basespawner and unit

---
+++ Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,51 @@
+﻿using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : NetworkBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+
+    [SyncVar]
+    private int currentHealth;
+
+    public event Action ServerOnDie;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        currentHealth = maxHealth;
+    }
+
+    [Server]
+    public void DealDamage(int damageAmount)
+    {
+        // stops taking damage when dead
+        if(currentHealth == 0) { return; }
+
+        // return highest number between damaged health and and 0, this stop health below 0
+        currentHealth = Mathf.Max(currentHealth - damageAmount, 0);
+
+        if(currentHealth != 0) { return; }
+
+        // raise dead event
+        ServerOnDie?.Invoke();
+
+        //
+        Debug.LogWarning("I died");
+
+    }
+
+    #endregion
+
+    ///
+
+
+    #region Client
+
+    #endregion
+
+}
--- Assets/Scripts/Units/UnitProjectile.cs
+++ Assets/Scripts/Units/UnitProjectile.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class UnitProjectile : NetworkBehaviour
 {
     [SerializeField] private Rigidbody rb = null;
+    [SerializeField] private int damageToDeal = 25;
     [SerializeField] private float projectileLifetime = 5f;
     [SerializeField] private float launchForce = 10f;
 
@@ -20,6 +21,29 @@ public class UnitProjectile : NetworkBehaviour
     {
         Invoke(nameof(DestroySelf), projectileLifetime);
     }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        // dont trigger friendly fire
+        if (other.TryGetComponent<NetworkIdentity>(out NetworkIdentity networkIdentity))
+        {
+            
+            if(networkIdentity.connectionToClient == connectionToClient) { return; }
+
+        }
+
+        // Damage enemies
+        if(other.TryGetComponent<Health>(out Health health))
+        {
+            health.DealDamage(damageToDeal);
+        }
+
+        // destroy the projectile
+        DestroySelf();
+        
+    }
+
 
     [Server]
     private void DestroySelf()
